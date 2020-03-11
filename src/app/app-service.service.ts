bc4a95ddcde4f9f0194d6df4a557e54d2d8e1d12
Sooraj <Sooraj@B3-SOORAJ-WIN10>
2020-03-11 15:18:19 -0400

    Added modal to show when the api fetch fails

--- src/app/app-service.service.ts
+++ src/app/app-service.service.ts
@@ -1,23 +1,19 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 
 @Injectable()
 export class AppServiceService {
   private api = "https://a18fda49-215e-47d1-9dc6-c6136a04a33a.mock.pstmn.io";
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient) {}
 
-   }
+  getCommunity() {
+    return this.http.get(this.api + "/communities");
+  }
+  getCommunityFailed() {
+    return this.http.get(this.api + "/communities1");
+  }
 
-   getCommunity()
-   {
-    
-     return this.http.get(this.api+"/communities");
-
-   }
-
-   getHome()
-   {
-     return this.http.get(this.api+"/homes");
-   }
-
+  getHome() {
+    return this.http.get(this.api + "/homes");
+  }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,55 +1,119 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, ViewChild } from "@angular/core";
+
 import { AppServiceService } from "../app/app-service.service";
 
 @Component({
   selector: "app-root",
+
   templateUrl: "./app.component.html",
+
   styleUrls: ["./app.component.css"]
 })
 export class AppComponent implements OnInit {
-  title = "app";
   communities: any;
   homes: any;
   showLoader: boolean = true;
+  showModal: boolean = true;
+
+  subTitle = "Please click on retry to retrieve data.";
+
   defaultLink = "/assets/sample-image.jpg";
 
+  @ViewChild("myModal") myModal;
+
   constructor(private appService: AppServiceService) {}
+
   ngOnInit() {
-    this.appService.getCommunity().subscribe(value => {
-      this.communities = Object.values(value).sort((obj1, obj2) => {
-        if (obj1.name > obj2.name) {
-          return 1;
-        }
+    this.getCommunityData();
+  }
 
-        if (obj1.name < obj2.name) {
-          return -1;
-        }
+  getCommunityData() {
+    this.appService.getCommunity().subscribe(
+      value => {
+        this.communities = Object.values(value).sort((obj1, obj2) => {
+          if (obj1.name > obj2.name) {
+            return 1;
+          }
 
-        return 0;
-      });
-      this.getHousePrices();
-    });
+          if (obj1.name < obj2.name) {
+            return -1;
+          }
+
+          return 0;
+        });
+        this.getHousePrices();
+      },
+      error => {
+        this.myModal.nativeElement.className = "modal show";
+      }
+    );
+  }
+
+  getCommunityDataFailed() {
+    this.appService.getCommunityFailed().subscribe(
+      value => {
+        this.communities = Object.values(value).sort((obj1, obj2) => {
+          if (obj1.name > obj2.name) {
+            return 1;
+          }
+
+          if (obj1.name < obj2.name) {
+            return -1;
+          }
+
+          return 0;
+        });
+
+        this.getHousePrices();
+      },
+      error => {
+        this.closeModal();
+      }
+    );
   }
 
   getHousePrices() {
-    this.appService.getHome().subscribe(value => {
-      this.homes = Object.values(value);
-      this.communities.forEach(community => {
-        let price = 0;
-        let numOfHouses = 0;
-        this.homes.forEach(home => {
-          if (community.id == home.communityId) {
-            numOfHouses = numOfHouses + 1;
-            price = price + home.price;
+    this.appService.getHome().subscribe(
+      value => {
+        this.homes = Object.values(value);
+
+        this.communities.forEach(community => {
+          let price = 0;
+
+          let numOfHouses = 0;
+
+          this.homes.forEach(home => {
+            if (community.id == home.communityId) {
+              numOfHouses = numOfHouses + 1;
+
+              price = price + home.price;
+            }
+          });
+
+          let avg = price / numOfHouses;
+
+          if (avg) {
+            community["avgPrice"] = avg;
           }
         });
-        let avg = price / numOfHouses;
-        if (avg) {
-          community["avgPrice"] = avg;
-        }
-      });
 
-      this.showLoader = false;
-    });
+        this.showLoader = false;
+      },
+      error => {
+        this.closeModal();
+      }
+    );
+  }
+
+  retryDataFetch() {
+    this.myModal.nativeElement.className = "modal fade";
+    this.showLoader = true;
+    this.getCommunityData();
+  }
+
+  closeModal() {
+    this.myModal.nativeElement.className = "modal show";
+
+    this.showLoader = false;
   }
 }
