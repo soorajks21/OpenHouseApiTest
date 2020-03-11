4ee54975a55450f0c9f20da92215176a6624b71c
Sooraj <Sooraj@B3-SOORAJ-WIN10>
2020-03-11 10:17:18 -0400

    Removed console.log stmt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,60 +1,50 @@
-import { Component, OnInit } from '@angular/core';
-import { AppServiceService } from '../app/app-service.service';
+import { Component, OnInit } from "@angular/core";
+import { AppServiceService } from "../app/app-service.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent implements OnInit {
-  title = 'app';
-  communities:any;
-  homes:any;
-constructor(private appService:AppServiceService)
-{
-
-}
+  title = "app";
+  communities: any;
+  homes: any;
+  constructor(private appService: AppServiceService) {}
   ngOnInit() {
-   this.appService.getCommunity().subscribe(value=>
-    {
-      this.communities = Object.values(value).sort((obj1, obj2)=>{
+    this.appService.getCommunity().subscribe(value => {
+      this.communities = Object.values(value).sort((obj1, obj2) => {
         if (obj1.name > obj2.name) {
           return 1;
-      }
-  
-      if (obj1.name < obj2.name) {
-          return -1;
-      }
+        }
 
-      return 0;
+        if (obj1.name < obj2.name) {
+          return -1;
+        }
+
+        return 0;
       });
       this.getHousePrices();
     });
   }
 
-  getHousePrices()
-  {
-    this.appService.getHome().subscribe(value=>
-      {
-        this.homes =  Object.values(value);
-        console.log("this.homes",this.homes);
-        console.log("this.communities", this.communities);
-        this.communities.forEach(community => { 
-          let price =0;
-          let numOfHouses=0;
-          this.homes.forEach(home => {
-            if(community.id==home.communityId)
-            {
-              numOfHouses= numOfHouses+1;
-              price=price+home.price;
-            }
-          });
-          let avg = price/numOfHouses;
-          if(avg)
-          {
-            community['avgPrice']=avg;
+  getHousePrices() {
+    this.appService.getHome().subscribe(value => {
+      this.homes = Object.values(value);
+      this.communities.forEach(community => {
+        let price = 0;
+        let numOfHouses = 0;
+        this.homes.forEach(home => {
+          if (community.id == home.communityId) {
+            numOfHouses = numOfHouses + 1;
+            price = price + home.price;
           }
         });
+        let avg = price / numOfHouses;
+        if (avg) {
+          community["avgPrice"] = avg;
+        }
       });
+    });
   }
 }
